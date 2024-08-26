081bb2916683300bafe97bfcc6592b5a54663ca2
moses <mosesowino1610@gmail.com>
2024-08-26 18:05:59 +0300

    removed leaderboard background color

--- client/src/components/UI/PoemsSection.js
+++ client/src/components/UI/PoemsSection.js
@@ -40,7 +40,7 @@ const PoemsSection = forwardRef((props, ref) =>{
     return(
         <Grid ref={ref} container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3, lg: 3, xl:3 }}  className=" w-screen min-h-screen mx-0 bg-secondary">
             <Grid item xs={11} sm={7} md={8} lg={9} xl={8}  className=" mx-auto mb-2 h-screen overflow-y-scroll">
-                <Typography variant="h5"  className="text-center font-bold">Poems</Typography>
+                <Typography variant="h5" color="white" className="text-center font-bold">Poems</Typography>
                 <Grid container spacing={2} className=" mt-8 h-fit -z-10 ">
                     {
                         props.poemData.map((poem)=>(
@@ -55,8 +55,8 @@ const PoemsSection = forwardRef((props, ref) =>{
                 </Grid>
             </Grid>
 
-            <Grid item xs={11} sm={4} md={3} lg={2} xl={3}  className=" w-auto p-1 mx-auto h-fit mt-8 border-2 border-lg bg-red-200 rounded-md">
-                <Typography variant="h5" color="white" className="Block mb-2 text-center font-bold text-black">Leaderboard</Typography>
+            <Grid item xs={11} sm={4} md={3} lg={2} xl={3}  className=" w-auto p-1 mx-auto h-fit mt-8 border-2 border-lg">
+                <Typography variant="h5" color="white" className="Block mb-2 text-center font-bold">Leaderboard</Typography>
                 {props.poemData.map((poem) => (
                     <Card className="px-2 py-1 my-3 m-2 rounded-md text-black bg-white flex justify-between">
                         <Typography variant="p">{poem.poemdata.title}</Typography>
